0fcf244fa34a0e3194f2411909e67ba9cfb077f7
Vishal <vishalpanchal1828vp@gmail.com>
2024-03-26 02:12:42 +0530

    inital commit, design login and signup page

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Login from "./components/pages/auth/Login";
+import Signup from "./components/pages/auth/Signup";
+
+function App() {
+  return (
+    <>
+      {/* <Login /> */}
+      <Signup />
+    </>
+  );
+}
+
+export default App;
