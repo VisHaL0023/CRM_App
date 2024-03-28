e882285042806e6763ed421ecf00400fa51cc48c
Vishal <vishalpanchal1828vp@gmail.com>
2024-03-29 02:33:53 +0530

    added line chart custom

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -32,7 +32,11 @@ ChartJS.register(
 
 const Home = () => {
   const [ticketsState] = useTickets();
-  const [openTickets, setOpenTickets] = useState({});
+  const [ticketsChartData, setTicketsChartData] = useState({
+    openTickets: [],
+    inProgressTickets: [],
+    resolvedTickets: [],
+  });
 
   function getPercentage(ticketStatus) {
     const totalTickets = ticketsState.downloadedTickets.length;
@@ -81,31 +85,98 @@ const Home = () => {
   };
 
   const lineChartData = {
-    labels: Object.keys(openTickets),
+    labels: Object.keys(ticketsChartData.openTickets),
     fontColor: "white",
     datasets: [
       {
         label: "Open Tickets data",
-        data: Object.values(openTickets),
+        data: Object.values(ticketsChartData.openTickets),
         borderColor: "rgb(255, 99, 132)",
+      },
+      {
+        label: "In Progress Tickets data",
+        data: Object.values(ticketsChartData.inProgressTickets),
+        borderColor: "rgb(53, 162, 235)",
+      },
+      {
+        label: "Resolved Tickets data",
+        data: Object.values(ticketsChartData.resolvedTickets),
+        borderColor: "rgb(245, 205, 95)",
+        borderWidth: 4,
       },
     ],
   };
 
-  useEffect(() => {
-    console.log(ticketsState);
+  function processTickets() {
+    // Fetch the current Date
+    const currentDate = new Date();
+    // Calculate the 10th date from today
+    const tenthDayFromToday = new Date();
+    tenthDayFromToday.setDate(currentDate.getDate() - 10);
+
+    // Process all the tickets
     if (ticketsState.ticketList.length > 0) {
+      // Prepare two localobjects to act as frequency map
       let openTicketsData = {};
+      let inProgressTicketsData = {};
+      let resolvedTicketsData = {};
+
+      // Initialise the frequency map with default value 0 for the last 10 days
+      for (let i = 0; i < 10; i++) {
+        // Get the ith day from today
+        const dateObject = new Date();
+        dateObject.setDate(currentDate.getDate() - i);
+        /**
+         * dateObject.toLocaleDateString() -> gives us string in the format DD/MM/YYYY
+         * Convert this to YYYY-MM-DD
+         */
+        openTicketsData[
+          dateObject.toLocaleDateString().split("/").reverse().join("-")
+        ] = 0;
+        inProgressTicketsData[
+          dateObject.toLocaleDateString().split("/").reverse().join("-")
+        ] = 0;
+        resolvedTicketsData[
+          dateObject.toLocaleDateString().split("/").reverse().join("-")
+        ] = 0;
+      }
+
+      // Process all the tickets one by one
       ticketsState.ticketList.forEach((ticket) => {
+        // Get the date part from the tickets by removing everything post the character T
         const date = ticket.createdAt.split("T")[0];
-        if (ticket.status == "open") {
-          openTicketsData[date] = !openTicketsData[date]
-            ? 1
-            : openTicketsData[date] + 1;
+        const ticketDate = new Date(ticket.createdAt);
+        // If ticket is open and lies in the last 10 days add it
+        if (ticket.status == "open" && ticketDate >= tenthDayFromToday) {
+          openTicketsData[date] = openTicketsData[date] + 1;
+        }
+
+        // If ticket is inProgress and lies in the last 10 days add it
+        if (ticket.status == "inProgress" && ticketDate >= tenthDayFromToday) {
+          inProgressTicketsData[date] = inProgressTicketsData[date] + 1;
+        }
+
+        // If ticket is resolved and lies in the last 10 days add it
+        if (ticket.status == "resolved" && ticketDate >= tenthDayFromToday) {
+          resolvedTicketsData[date] = resolvedTicketsData[date] + 1;
+        }
+        if (ticket.status == "resolved") {
+          console.log(ticket);
         }
       });
-      setOpenTickets(openTicketsData);
+      //  update the state
+      console.log(resolvedTicketsData);
+      setTicketsChartData({
+        openTickets: openTicketsData,
+        inProgressTickets: inProgressTicketsData,
+        resolvedTickets: resolvedTicketsData,
+      });
     }
+  }
+
+  useEffect(() => {
+    console.log(ticketsState);
+    processTickets();
   }, [ticketsState.ticketList]);
 
   return (
