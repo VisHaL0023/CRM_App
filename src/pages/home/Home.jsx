de6c9c87144c0e785888507bba8b27f768657879
Vishal <vishalpanchal1828vp@gmail.com>
2024-03-29 01:11:35 +0530

    added line chart

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,3 +1,16 @@
+import {
+  ArcElement,
+  CategoryScale,
+  Chart as ChartJS,
+  Legend,
+  LinearScale,
+  LineElement,
+  PointElement,
+  Title,
+  Tooltip,
+} from "chart.js";
+import { useEffect, useState } from "react";
+import { Line, Pie } from "react-chartjs-2";
 import { BsFillPencilFill } from "react-icons/bs";
 import { MdCancel, MdOutlineDoneAll, MdPending } from "react-icons/md";
 import { TbProgressBolt } from "react-icons/tb";
@@ -6,8 +19,20 @@ import Card from "../../components/Card";
 import useTickets from "../../hooks/useTickets";
 import HomeLayout from "../../layouts/HomeLayout";
 
+ChartJS.register(
+  ArcElement,
+  Legend,
+  Title,
+  Tooltip,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement
+);
+
 const Home = () => {
   const [ticketsState] = useTickets();
+  const [openTickets, setOpenTickets] = useState({});
 
   function getPercentage(ticketStatus) {
     const totalTickets = ticketsState.downloadedTickets.length;
@@ -40,6 +65,48 @@ const Home = () => {
     const percentage = ((currTicket / totalTickets) * 100).toFixed(2);
     return percentage;
   }
+
+  const pieChartData = {
+    labels: Object.keys(ticketsState.ticketDistribution),
+    fontColor: "white",
+    datasets: [
+      {
+        label: "Tickets data",
+        data: Object.values(ticketsState.ticketDistribution),
+        backgroundColor: ["yellow", "red", "green", "blue", "purple"],
+        borderColor: ["yellow", "red", "green", "blue", "purple"],
+        borderWidth: 1,
+      },
+    ],
+  };
+
+  const lineChartData = {
+    labels: Object.keys(openTickets),
+    fontColor: "white",
+    datasets: [
+      {
+        label: "Open Tickets data",
+        data: Object.values(openTickets),
+        borderColor: "rgb(255, 99, 132)",
+      },
+    ],
+  };
+
+  useEffect(() => {
+    console.log(ticketsState);
+    if (ticketsState.ticketList.length > 0) {
+      let openTicketsData = {};
+      ticketsState.ticketList.forEach((ticket) => {
+        const date = ticket.createdAt.split("T")[0];
+        if (ticket.status == "open") {
+          openTicketsData[date] = !openTicketsData[date]
+            ? 1
+            : openTicketsData[date] + 1;
+        }
+      });
+      setOpenTickets(openTicketsData);
+    }
+  }, [ticketsState.ticketList]);
 
   return (
     <HomeLayout>
@@ -102,6 +169,16 @@ const Home = () => {
           </Card>
         </div>
       )}
+      <div className="mt-10 flex justify-center items-center gap-10">
+        <div className="w-80 h-80 ">
+          <Pie data={pieChartData} />
+        </div>
+      </div>
+      <div className="mt-10 flex justify-center items-center gap-10">
+        <div className="w-[40rem] h-[40rem]">
+          <Line data={lineChartData} />
+        </div>
+      </div>
     </HomeLayout>
   );
 };
